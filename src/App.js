1dbea1b5f91bc808f4446bc192def7443c93e67f
Nathan Fowler <luxurysandbox@gmail.com>
2019-05-06 16:26:15 -1000

    added setTimeout to mimic api call, added dummy-store data, created helper function export document

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './Header/Header';
 import './App.css';
+import DATA from './dummy-store'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       
+       notes: [],
+       folders: [],
+       err: null
     }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        folders: DATA.folders,
+        notes: DATA.notes
+      })
+    }, 600);
   }
   
   render() {
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 export default function Header(props) {
   return (
     <header className="App__header">
-      <h1>{props.title}</h1>
+      <h1><Link to="/">{props.title}</Link></h1>
     </header>
   )
 }
