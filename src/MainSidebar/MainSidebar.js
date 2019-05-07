d9abb38a7e558163ca6d6c397dba1b787dee999a
Nathan Fowler <luxurysandbox@gmail.com>
2019-05-06 17:56:00 -1000

    added MainSidebar rendering component

---
+++ src/MainSidebar/MainSidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './MainSidebar.css'
+
+function MainSidebar(props) {
+  const { folders, notes } = props;
+  console.log( folders, notes);
+
+  return (
+    <div className='MainSidebar'>
+      <ul className='MainSidebar__list'>
+        {folders.map(folder =>
+          <li key={folder.id}>
+            <NavLink
+              className="MainSidebar__folder-link"
+              to={`/folder/${folder.id}`}
+            >
+             {folder.name} 
+            </NavLink>
+          </li>  
+        )}
+      </ul>
+    </div>
+  )
+}
+
+
+export default MainSidebar
+
+
