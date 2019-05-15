a4c2eeae8be88b66779b881d2a4b5fb53f7af7bf
Nathan Fowler <luxurysandbox@gmail.com>
2019-05-14 15:37:10 -1000

    added notes context

---
+++ src/notesContext.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotesContext = React.createContext({
+  folders: [],
+  notes: [],
+  addNote: () => {},
+  deleteNote: () => {},
+});
+
+export default NotesContext
